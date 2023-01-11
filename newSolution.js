6d01178c8dc6f99a7b74c7305cb2a3e661459097
jacky <jacky@metallurgietech.com>
2023-01-11 13:53:59 +0800

    更新

--- newSolution.js
+++ newSolution.js
@@ -1,26 +1,31 @@
- //1.外圈從第一row開始跑；然後使用check函式放皇后，
-   //如果在該row的最後一col check結果也是false，表示無解
-//2. check函式 (傳入Queen的位置,檢查是否有和其他皇后相撞)
-  //只要檢查 其上一Row開始的Rows 有無任一在他米字範圍內的皇后即可
-  //回傳true或false
-  //true的話，往下一row移動  ； false的話往右邊的col移動
-  
-//樣式修改變數
+  //樣式修改變數
 const QUEENSYMBOL = 'Q' //主要符號
 const SPACESYMBOL = '.' //次要符號
 const noVacanciesSymbol = 'X'
 const maxPuzzleSize = 10
 
-//切換輸出結果: 
-puzzleOutput(4) 
 //methods
 
 //工具方法
-function getSplitNumberStr(number){
-    if(number<10) number = '0'+ number
-    return String(number).split("")
+function verify(puzzleNumber){
+    if(puzzleNumber > maxPuzzleSize) {
+        console.log(`請勿輸入超過${maxPuzzleSize}`)
+        return false
+    }
+    if( typeof(puzzleNumber)!=='number' ) {
+        console.log('請輸入數字')
+        return false
+    }
+    if( !Number.isInteger(puzzleNumber) || puzzleNumber <= 0 ) {
+        console.log('請輸入正整數')
+        return false
+    }
+    if( (puzzleNumber % 2) !==0  ) {
+        console.log('請輸入偶數')
+        return 
+    }   
+    return true
 }
-
 /**
  * 初始化棋盤
  * @param {number} puzzleNumber 
@@ -37,155 +42,52 @@ function initPuzzle(puzzleNumber){
 }
 
 /**
- * 設置皇后米字範圍不能放置其他皇后
- * @param {Array} queenPuzzle :傳入棋盤陣列
- * @param {number} queenIndex : queen所在位置的index
+ * 棋盤產生器
+ * 畫出一行的棋盤
+ * @param {*} puzzleNumber 必填，代表 n-puzzle 的 n
+ * @param {*} placementIndex 必填，指示Queen要放在哪個位置
+ * @param {*} queenSymbol 必填，Queen 的符號樣式
+ * @param {*} spaceSymbol 必填，空格 的符號樣式
+ * @returns string ， 返回一行棋盤字串
  */
-function setNoVacancies(queenPuzzle,queenIndex){
-    const [ queenRowIndex, queenColIndex ] = getSplitNumberStr(queenIndex)
-    const puzzleRows = queenPuzzle.length
-    const puzzleCols = queenPuzzle[0].length
-     //2.將其米字路徑範圍上的都設置為"不能擺放"
-        //如:第一顆02
-        //該row全部都不能放(橫)
-        for( let i = 0 ; i< puzzleCols ;i++){
-            //Q的地方或是已經標示為沒空位的則不用再畫一次
-            if( i === Number(queenColIndex) || (queenPuzzle[queenRowIndex][i] === noVacanciesSymbol)) continue
-            queenPuzzle[queenRowIndex][i] = noVacanciesSymbol
-        }
-        //每一row的第2顆都不能放(直)
-        for( let i = 0 ; i< puzzleRows ;i++){
-            //Q的地方或是已經標示為沒空位的則不用再畫一次
-            if( i === Number(queenRowIndex) || queenPuzzle[i][queenColIndex] === noVacanciesSymbol) continue
-            queenPuzzle[i][queenColIndex] = noVacanciesSymbol
-        }
-        //其右上到左下的路徑都不能放
-        for( let i = 0 ; i< puzzleRows ;i++){
-            //Q的地方或是已經標示為沒空位的則不用再畫一次
-            let col = Number(queenColIndex) + (Number(queenRowIndex)-i)
-            if(col >= puzzleCols) continue //超出表格也不用畫
-            if( i === Number(queenRowIndex) || queenPuzzle[i][col] === noVacanciesSymbol) continue
-            //畫符號
-            queenPuzzle[i][Number(queenColIndex) + (Number(queenRowIndex)-i)] = noVacanciesSymbol
-        }
-        //其左上到右下的路徑都不能放
-        for( let i = 0 ; i< puzzleRows ;i++){
-            let col = Number(queenColIndex) + (i-Number(queenRowIndex))
-            if(col >= puzzleCols) continue //超出表格也不用畫
-            //Q的地方或是已經標示為沒空位的則不用再畫一次
-            if( i === Number(queenRowIndex) || queenPuzzle[i][col] === noVacanciesSymbol) continue
-            //畫符號
-            queenPuzzle[i][Number(queenColIndex) + (i-Number(queenRowIndex))] = noVacanciesSymbol
-        }
-}
+function drawPuzzle(puzzleNumber, placementIndex, queenSymbol, spaceSymbol){
+    //把queen放在第index個，然後剩下的用second填滿
+    let result = ''
+    for ( let i = 0 ; i < puzzleNumber ; i++){
+        if( i === placementIndex ) result += queenSymbol
+        else result += spaceSymbol
+    }
 
-/**
- * 遞迴-為棋盤設置每一行的皇后
- * @param {number} puzzleNumber 
- * @param {Array} queenPuzzle 
- * @param {number} puzzleRow 
- * @param {number} totalQueen 皇后總數，每次遞迴都會回傳累積的總數
- * @returns {number} 會把最後的皇后數量+總，讓外面判斷是否有解
- */
-function setRowQueen(puzzleNumber, queenPuzzle, puzzleRow, totalQueen = 0) {
-    //終止條件
-    if(puzzleRow > puzzleNumber-1) return totalQueen
-    
-    //遞迴主體
-    queenPuzzle[puzzleRow].forEach( (symbol,index) =>{
-        //遇到皇后++一下
-        if( symbol ===  QUEENSYMBOL) totalQueen ++
-
-        //若都沒有皇后、並且有空位
-        if( symbol !== noVacanciesSymbol && symbol !== QUEENSYMBOL) { 
-            queenPuzzle[puzzleRow][index] = QUEENSYMBOL
-            setNoVacancies( queenPuzzle, Number(puzzleRow+''+index))
-            totalQueen ++
-            return 
-        }
-        
-    } )
-    //驅動遞迴
-    puzzleRow++
-    return setRowQueen(puzzleNumber, queenPuzzle, puzzleRow, totalQueen)
-}
-
-/**
- * 棋盤 產生器 (會產生一種解)
- * 負責產生每個位置的Queen要擺在個index的列表清單
- * @param {number} puzzleNumber 必填，代表 n-puzzle 的 n
- * @param {number} firstQueenIndex 必填，選擇第一個queen要擺放的位置
- * @returns {Array<number>}[queenIndexArray] 返回一個每行皇后擺放位置的清單 ( 一種解 )
- */
-function queenPuzzleMaker(puzzleNumber, firstQueenIndex = 0){
-    const [ queenRowIndex, queenColIndex ] = getSplitNumberStr(firstQueenIndex)
-    let queenPuzzle = initPuzzle(puzzleNumber)
-    //陣列製作步驟 :
-    //1.放置第一顆皇后，並將其米字路徑設置為不能使用
-    queenPuzzle[queenRowIndex][queenColIndex] = QUEENSYMBOL
-    setNoVacancies(queenPuzzle,firstQueenIndex)
-    //2.開始遞迴queenPuzzleMaker在下一行可以擺放的位置上放皇后
-    let queenNumber = setRowQueen(puzzleNumber, queenPuzzle,0)
-
-    //如果有解回傳陣列，否則回傳null
-    if( queenNumber === puzzleNumber ) return queenPuzzle
-    return null
-}
-
-/**
- * 依照二維陣列畫出一種棋盤解
- * @param { number } puzzleNumber 必填，代表 n-puzzle 的 n
- * @param { number } firstQueenIndex 必填
- * @returns string ，返回最終 puzzle 輸出
- */
-function solutionMaker(puzzleNumber, firstQueenIndex =0){
-    const newLinrSymbol = '\n'
-    let result = `//solution 1 :${newLinrSymbol}`
-
-    const queenIndexArray = queenPuzzleMaker( puzzleNumber, firstQueenIndex )
-    if(!queenIndexArray) return '無解'
-    queenIndexArray.forEach( row => { 
-        let rowResult = ''
-        row.forEach( (symbol,index) =>{
-            if( !symbol ) rowResult += SPACESYMBOL
-            else if( symbol === noVacanciesSymbol) rowResult += SPACESYMBOL
-            // else if( symbol === noVacanciesSymbol) rowResult += noVacanciesSymbol  //debug專用
-            else if( symbol === QUEENSYMBOL ) rowResult += QUEENSYMBOL
-            if(index === puzzleNumber-1) rowResult += newLinrSymbol
-        } )
-        result += rowResult
-    })
     return result
 }
 
-
+//演算法
 /**
- * 輸出 puzzle solution 的 main function
- * 
- * @param {number} puzzleNumber 必填，代表 n-puzzle 的 n
- * @param { number } firstQueenIndex 選填，可以指定第一顆皇后的擺放位置 ( 若index範圍不在棋盤反圍內、無法得解都可能造成無法得解 )
- * @returns string ， 返回最終 puzzle 輸出
+ * 用來檢查該格未置可否放置皇后
+ * @param {Object} position ，位置物件: { row:行, col:列 }
+ * @param {Array} puzzle , 棋盤二維陣列
+ * @returns {boolean} 回傳true表示該格可以放皇后
  */
-function puzzleOutput( puzzleNumber, firstQueenIndex = 0 ){
-    if(puzzleNumber > maxPuzzleSize) return console.log(`請勿輸入超過${maxPuzzleSize}`)
-    const [ queenRowIndex, queenColIndex ] = getSplitNumberStr(firstQueenIndex)
-    if(firstQueenIndex<0 || queenRowIndex > puzzleNumber-1 || queenColIndex > puzzleNumber-1) return console.log('旗子擺放位置超出範圍')
-    if( typeof(puzzleNumber)!=='number' ) return console.log('請輸入數字')
-    if( !Number.isInteger(puzzleNumber) || puzzleNumber <= 0 ) return console.log('請輸入正整數')
-    if( (puzzleNumber % 2) !==0  ) return console.log('請輸入偶數')
-
-    console.log(solutionMaker(puzzleNumber, firstQueenIndex))
-    //要使用遞迴來求解
-    for(let i = 0 ; i< puzzleNumber.length ; i++ ){
-        
-    }
+function hasQueenPosition(position,puzzle) {
+    const { row, col } = position
+    //2. check函式 (傳入Queen的位置,檢查是否有和其他皇后相撞)
+        //只要檢查 其上一Row開始的Rows 有無任一在他米字範圍內的皇后即可
+        //回傳true或false
+    return false
+}
+function getAllpuzzleSolution( puzzleNumber ){
+    const puzzleSolutions = []  //裝解答
+    if(!verify(puzzleNumber)) return 
+     //1.外圈從第一row開始跑；然後使用check函式放皇后，
+        //check回傳true的話，往下一row移動 ； false的話往右邊的col移動
+        //如果在該row的最後一col check結果也是false，表示無解
+    //2.最後照puzzleSolutions印出所有解答
 }
 
 //test
-// console.log(puzzleOutput('aa'))  //"請輸入數字"
-
-// console.log(puzzleOutput(-1))     //"請輸入正整數"
-// console.log(puzzleOutput(0))    //"請輸入正整數"
-// console.log(puzzleOutput(5.5))     //"請輸入正整數"
-// console.log(puzzleOutput(-5.5))     //"請輸入正整數"
-// console.log(puzzleOutput(9))     //'請輸入偶數'
+// getAllpuzzleSolution('aa')  //"請輸入數字"
+// getAllpuzzleSolution(-1)     //"請輸入正整數"
+// getAllpuzzleSolution(0)    //"請輸入正整數"
+// getAllpuzzleSolution(5.5)     //"請輸入正整數"
+// getAllpuzzleSolution(-5.5)     //"請輸入正整數"
+// getAllpuzzleSolution(9)     //'請輸入偶數'
