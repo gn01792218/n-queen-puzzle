9dbc7102db6c3e1cfa082bff84da95a8303ae181
jacky <jacky@metallurgietech.com>
2023-01-13 09:54:15 +0800

    撰寫核心遞迴邏輯(但是有BUG!!!)

--- newSolution.js
+++ newSolution.js
@@ -26,20 +26,6 @@ function verify(puzzleNumber){
     }   
     return true
 }
-/**
- * 初始化棋盤
- * @param {number} puzzleNumber 
- * @returns {Array} 返回一個 queenPuzzle 陣列
- */
-function initPuzzle(puzzleNumber){
-    const queenPuzzle = Array.from(Array(puzzleNumber),()=> new Array(puzzleNumber)) 
-    queenPuzzle.forEach( row => {
-        for(let i = 0 ; i <row.length ;i++ ){
-            row[i] = null
-        }
-    })
-    return queenPuzzle
-}
 
 /**
  * 棋盤產生器
@@ -66,7 +52,7 @@ function drawPuzzle(puzzleNumber, placementIndex, queenSymbol, spaceSymbol){
  * 檢查該格位置可否放置皇后 :
  * 只要檢查 其上一Row以上的Rows中 有無任一在其 米字範圍內 的皇后即可
  * @param {Array} currentposition ，[rowIndex, colIndex]
- * @param {Array} solutionArr , 每一行皇后位置的清單
+ * @param {Array} solutionArr , 每一行皇后位置的清單 ( 長度為n，每個index代表row，值表示皇后所在的col )
  * @returns {boolean} 回傳true表示該格可以放皇后
  */
 function isSafe(currentposition, solutionArr) {
@@ -84,18 +70,28 @@ function isSafe(currentposition, solutionArr) {
     }
     return true
 }
+function findQueenLoop(puzzleNumber,currentRow,solutionArr,puzzleSolutions){
+    console.log('開啟',currentRow,'的搜尋')
+    //開始每一col的查找
+    for(let col = 0 ; col < puzzleNumber ; col++ ){
+        console.log('搜尋',col)
+        if(!isSafe([ currentRow, col ], solutionArr)) continue
+        else{ //找到安全位置
+            solutionArr[currentRow] = col //標示皇后位置
+            if( currentRow !== puzzleNumber - 1 ) {  //不是最後一行的話
+                console.log('找到往下',)
+                findQueenLoop(puzzleNumber,++currentRow,solutionArr) //繼續往下找
+            }
+            else puzzleSolutions.push(solutionArr) //走完全部，把解推入解方陣列中
+        }
+    }
+    console.log(currentRow,'迴圈結束')
+}
 function getAllpuzzleSolution( puzzleNumber ){
-    const puzzleSolutions = []  //裝解答
     if(!verify(puzzleNumber)) return 
-    // let puzzle = initPuzzle(puzzleNumber)
+    const puzzleSolutions = []  //裝解答
     const currentSolution = [] // --> 專門儲存皇后所在位置的列表清單，例如[1,3,2,5,6,7,4,0]，index代表row，值代表col
-    
-    //一行一行放皇后 
-    //每一行都會檢查每一格是否可以放皇后 (目標 將皇后擺放在第一個安全位置上 ) 
-       //若該格可以放，將該col位置紀錄起來，currentSolution[row] = col ==> 然後繼續找下一格!!!
-       //若該格不能放，則繼續跑下一格
-       //若能跑到currentSolution[最後一個]，就是有解答，直接推送到解答組裡面
-        
+    findQueenLoop(puzzleNumber,0,currentSolution,puzzleSolutions)
     //2.最後照puzzleSolutions印出所有解答
     console.log(puzzleSolutions)
 }
